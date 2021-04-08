f2bbd45ad14de5818fd8636bd3178f3674302591
Yahir Rendon <33650498+yahirRendon@users.noreply.github.com>
2021-04-08 01:16:30 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
-
 var goToDesign = document.getElementById('designButton');
+var goToAlgo = document.getElementById('algoButton');
+var goToData = document.getElementById('dataButton');
 
 if(goToDesign){
     goToDesign.addEventListener("click", openDesign);
 }
 
+if(goToAlgo){
+    goToAlgo.addEventListener("click", openAlgo);
+}
+
+if(goToData){
+    goToData.addEventListener("click", openData);
+}
+
+
 function openDesign() {
     window.location.href = "https://yahirrendon.github.io/design.html";
+}
+
+function openAlgo() {
+    window.location.href = "https://yahirrendon.github.io/algo.html";
+}
+
+function openData() {
+    window.location.href = "https://yahirrendon.github.io/database.html";
 }
