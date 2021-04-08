ff4c3fa10055dfa4e27e876f2fabe504dee8c397
Yahir Rendon <33650498+yahirRendon@users.noreply.github.com>
2021-04-08 16:25:50 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+/**
+ * Author: 	Yahir Rendon 
+ * Date:	April 2021
+ * Project: ePortfolio
+ * 
+ * scripts
+ */
+
 var goToDesign = document.getElementById('designButton');
 var goToAlgo = document.getElementById('algoButton');
 var goToData = document.getElementById('dataButton');
 
+/**
+ * On windowload check navigation display
+ */
+window.onload = function () {
+	navigationBarToggle()
+}
+
+// check onClick element exists on page
 if(goToDesign){
     goToDesign.addEventListener("click", openDesign);
 }
@@ -14,15 +30,34 @@ if(goToData){
     goToData.addEventListener("click", openData);
 }
 
-
+// go to desired page
 function openDesign() {
-    window.location.href = "https://yahirrendon.github.io/design.html";
+    window.location.href = "design.html";
 }
 
 function openAlgo() {
-    window.location.href = "https://yahirrendon.github.io/algo.html";
+    window.location.href = "algo.html";
 }
 
 function openData() {
-    window.location.href = "https://yahirrendon.github.io/database.html";
+    window.location.href = "database.html";
+}
+
+/**
+ * Toggle the navigation bar between show/hide
+ */
+function navigationBarToggle() {
+	// on menuButton click check if navigation menu is hidden
+	// or not and do the opposite 
+	$("#menuButton").click(function () {
+		if ($('.home').css('display') == 'none') {
+            $(".home").css({ display: "inline" }).show();
+            $(".plan").css({ display: "inline" }).show();
+			$(".about").css({ display: "inline" }).show();
+		} else {
+			$(".home").css({ display: "none" }).hide();
+            $(".plan").css({ display: "none" }).hide();
+            $(".about").css({ display: "none" }).hide();
+		}
+	})
 }
