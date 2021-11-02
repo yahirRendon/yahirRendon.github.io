ccda36fa384972a731feca5b6771e5f6462bc39f
Yahir Rendon <33650498+yahirRendon@users.noreply.github.com>
2021-11-02 13:12:40 -0700

    Create scripts_trominotris.js
    test sound

---
+++ ePortfolioSite/js/sketches/scripts_trominotris.js
@@ -0,0 +1,26 @@
+/**************************************************************
+ Project:  Trominotris
+ Author:   Yahir
+ Date:     November 2021
+ 
+ **************************************************************/
+
+var song;
+
+function preload() {
+    song = loadSound('assets/bensound-dreams.mp3')
+
+}
+
+function setup() {
+    createCanvas(400, 400)
+    background(200);
+}
+
+function draw() {
+
+}
+
+function keyPressed() {
+    song.play();
+}
